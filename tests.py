7e056d9a9b71f73109adaabdd03aa24f1f3ff3d1
Lisa Gray <gray7@pdx.edu>
2015-11-15 15:35:38 -0800

    fix grammar3; fix tests

--- tests.py
+++ tests.py
@@ -16,91 +16,95 @@ class GrammarTestCase(unittest.TestCase):
     def setUp(self):
         self.analyzer = GrammarAnalyzer("grammar.json")
 
+    # kind of a stupid test but meh.
     def test_should_be_instantiated(self):
         self.assertIsInstance(self.analyzer, GrammarAnalyzer)
         self.assertEqual(self.analyzer.stack, [self.analyzer.json["start"]])
         self.assertEqual(self.analyzer.input_buffer, [])
         self.assertIsNotNone(self.analyzer.json)
 
-    def test_first_grammar_fields(self):
+    def test_should_load_first_grammar_fields(self):
         self.assertEqual(self.analyzer.json["terminals"], ["a", "b", "#"])
         self.assertEqual(self.analyzer.json["start"], "S")
         self.assertEqual(self.analyzer.json["rules"][0], {'var': 'S', 'derives': "aTb"})
 
-    def test_isVariable(self):
+    def test_is_variable(self):
         self.analyzer.stack.append("S")
         self.assertTrue(self.analyzer.is_variable(self.analyzer.stack.pop()))
         self.analyzer.stack.append("X")
         self.assertFalse(self.analyzer.is_variable(self.analyzer.stack.pop()))
 
-    def test_isTerminal(self):
+    def test_is_terminal(self):
         self.analyzer.stack.append("a")
         self.analyzer.stack.append("S")
         self.assertFalse(self.analyzer.is_terminal(self.analyzer.stack.pop()))
         self.assertTrue(self.analyzer.is_terminal(self.analyzer.stack.pop()))
 
-    def test_isAcceptedIfStackAndBufferAreEmpty(self):
+    def test_is_accepted(self):
         self.analyzer.input_buffer = 'a'
         self.assertEqual('reject', self.analyzer.is_accepted())
         self.analyzer.input_buffer = ''
         self.analyzer.stack.pop()
         self.assertEqual('accept', self.analyzer.is_accepted())
 
-    def test_derive_rule(self):
+    def test_should_derive_correct_rule(self):
         self.assertEqual(self.analyzer.derive("S", "a"), "aTb")
         self.assertEqual(self.analyzer.derive("T", "#"), "#")
-        self.assertEqual(self.analyzer.derive("Y", "x"), None)
 
-    def test_analyze_accept(self):
+    def test_should_derive_None(self):
+        self.assertEqual(self.analyzer.derive("Y", "x"), None)
+        self.assertEqual(self.analyzer.derive("S", "x"), None)
+
+    def test_should_accept_grammar1(self):
         self.analyzer.input_buffer = "aa#bb"
         self.assertEqual("accept", self.analyzer.analyze())
 
-    def test_analyze_accept1(self):
+    def test_should_accept_grammar1_long(self):
         self.analyzer.input_buffer = "aaaaaa#bbbbbb"
         self.assertEqual("accept", self.analyzer.analyze())
 
-    def test_analyze_reject(self):
+    def test_should_reject_grammar1_input_buffer_empty(self):
         self.analyzer.input_buffer = "a#"
         self.assertEqual("reject", self.analyzer.analyze())
 
-    def test_analyze_reject2(self):
+    def test_should_reject_grammar1_no_rule(self):
         self.analyzer.input_buffer = "#"
         self.assertEqual("reject", self.analyzer.analyze())
 
-    def test_analyze_reject3(self):
+    def test_should_reject_grammar1_no_match_on_terminal(self):
         self.analyzer.input_buffer = "aa#cb"
         self.assertEqual("reject", self.analyzer.analyze())
 
-    def test_analyze_reject4(self):
-        self.analyzer.input_buffer = "aa#cc"
+    def test_should_reject_grammar1_no_match_on_terminal2(self):
+        self.analyzer.input_buffer = "aa#bc"
         self.assertEqual("reject", self.analyzer.analyze())
 
-    def test_analyze_grammar2_accept(self):
+    def test_should_accept_grammar2(self):
         self.analyzer = GrammarAnalyzer("grammar2.json")
         self.analyzer.input_buffer = "0101101010#0101011010"
         self.assertEqual("accept", self.analyzer.analyze())
 
-    def test_analyze_grammar2_reject(self):
+    def test_should_reject_grammar2_does_not_match_terminal(self):
         self.analyzer = GrammarAnalyzer("grammar2.json")
         self.analyzer.input_buffer = "0101101010#0101101010"
         self.assertEqual("reject", self.analyzer.analyze())
 
-    def test_analyze_grammar3_accept(self):
+    def test_should_accept_grammar3_short(self):
         self.analyzer = GrammarAnalyzer("grammar3.json")
         self.analyzer.input_buffer = "aa#bb#c#"
         self.assertEqual("accept", self.analyzer.analyze())
 
-    def test_analyze_grammar3_accept2(self):
+    def test_should_accept_grammar3_more_c(self):
         self.analyzer = GrammarAnalyzer("grammar3.json")
         self.analyzer.input_buffer = "aaaaa#bbbbb#ccccccccccccccc#"
         self.assertEqual("accept", self.analyzer.analyze())
 
-    def test_analyze_grammar3_accept3(self):
+    def test_should_accept_grammar3_more_a_b(self):
         self.analyzer = GrammarAnalyzer("grammar3.json")
         self.analyzer.input_buffer = "aaaaaaaaaaaaaa#bbbbbbbbbbbbbb#c#"
         self.assertEqual("accept", self.analyzer.analyze())
 
-    def test_analyze_grammar3_reject(self):
+    def test_should_reject_grammar3_incorrect_rule(self):
         self.analyzer = GrammarAnalyzer("grammar3.json")
-        self.analyzer.input_buffer = "a##c#"
+        self.analyzer.input_buffer = "##c#"
         self.assertEqual("reject", self.analyzer.analyze())
